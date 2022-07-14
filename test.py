0c244a0d3cc29b3642b4a0f589530d42c101eed3
H0cok <55365127+H0cok@users.noreply.github.com>
2022-07-14 13:26:10 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# import chess
+# import chess.pgn
+#
+# game = chess.pgn.Game()
+# game.headers["Event"] = "Example"
+# node = game.add_variation(chess.Move.from_uci("e2e4"))
+# node = node.add_variation(chess.Move.from_uci("e7e5"))
+game_storage = open("game_storage.txt", "a")
+game_storage.write("penis\n")
+game_storage.close()
